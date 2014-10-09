69498fc4f217bf6c8a3d5d63e455b96a16e78eb1
Adrian Ceballos <zzAdrianCeballos@gmail.com>
2014-10-09 11:14:00 -0700

    finished

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $pls = 3;
+        $name = "Adrian";
+        echo "Hi my name is ", $name;
+        if ($pls >= 4) {
+           echo"ayy"; 
+        } else {
+            echo " Three too good";
+        }
+        
+
+        // put your code here
+        ?>
+    </body>
+</html>
